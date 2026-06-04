0d4b01dc5a731b1df67b3a5f8ec7d6e5a0f23fe7
Maria Kuznetsova <dafna5566@gmail.com>
2026-06-04 23:06:37 +0300

    Fix SMTP port 587

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const crypto = require('crypto');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
